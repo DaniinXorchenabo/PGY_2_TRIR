2dd09e13a4186ae95c08d43406135ecc769fc7ee
DaniinXorchenabo <rkbcu@mail.ru>
2021-03-13 23:33:18 +0300

    values from data.js visible on start screen

--- laba_4_1/public_html/scripts/create_test.js
+++ laba_4_1/public_html/scripts/create_test.js
@@ -1,7 +1,7 @@
 function create_question_classes(count_questions, test_time){
     BaseQuestion.question_count = count_questions;
     var keys = [];
-    var got_form_types = ["radio"]
+    var got_form_types = ["radio"];
     for(var k in data_test){
         if (got_form_types.indexOf(data_test[k]['type']) !== -1 ){
             keys.push(k);
--- laba_4_1/public_html/scripts/script.js
+++ laba_4_1/public_html/scripts/script.js
@@ -8,7 +8,20 @@ var timer = [
     [0, 24], // 1 час (максимальное число в разряде  часов - 24),
     [0, 365]
 ];
-
+var keys = [];
+var got_form_types = ["radio"];
+for(var k in data_test){
+    if (got_form_types.indexOf(data_test[k]['type']) !== -1 ){
+        keys.push(k);
+    }
+}
+var max_question = keys.length;
+document.getElementById("count_question").max = max_question;
+document.getElementById("count_seekbar_qu").max = max_question;
+document.getElementById("count_question").value = document.getElementById("count_seekbar_qu").value;
+document.getElementById("tickmarks").innerHTML = Array.from(Array(max_question).keys()).reduce(function(a, b){
+    return a + "\n" + `<option value="${(b + 1)}" ${Math.floor((max_question - 1) / 2) === b || b === 0 || max_question - 1 === b?
+    'label="' + (b + 1) + '"': ""}>`; }, "");
 
 function set_seekbar(){
     // Устанавливает значение для ползунка количества вопросов
@@ -18,3 +31,4 @@ function set_seekbar(){
 function set_count_question(){
     document.getElementById("count_question").value = document.getElementById("count_seekbar_qu").value;
 }
+
